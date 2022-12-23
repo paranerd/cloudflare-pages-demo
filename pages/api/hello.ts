56e22249ee1408dc29b8a630268220b7e53758a8
paranerd <paranerd.development@gmail.com>
2022-12-23 20:45:16 +0100

    Updated hello API according to Cloudflare documentation

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+/* eslint-disable import/no-anonymous-default-export */
 
-type Data = {
-  name: string
-}
+export const config = {
+  runtime: 'experimental-edge',
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function () {
+  return new Response(JSON.stringify({ name: 'John Doe' }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 }
